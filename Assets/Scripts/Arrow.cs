41ec7af23ce999d9d615461ff63401eb1c1f3fdd
Andrey Panfilov <a.panfilov@nexters.com>
2016-08-08 15:42:32 +0300

    physics

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -14,7 +14,23 @@ public class Arrow : MonoBehaviour {
 
 	void Start() {
 		isFlying = true;
-		myRigidbody.AddForce (new Vector3(transform.forward.x * Force, transform.forward.y * Force, transform.forward.z * Force));
+		float initialZ = transform.position.z;
+		Vector3 force = new Vector3(transform.forward.x * Force, transform.forward.y * Force, transform.forward.z * Force);
+		myRigidbody.AddForce (force, ForceMode.VelocityChange);
+		float angle = Vector3.Angle (transform.forward, Vector3.forward);
+		float vel0sq = Mathf.Pow (force.magnitude, 2.0f);
+		float radAngle = Mathf.Deg2Rad * angle;
+		float h = transform.position.y;
+		Debug.Log (h);
+			
+		float secondPart = 1 + Mathf.Sqrt (1 + (2 * Physics.gravity.magnitude * h) / (vel0sq * Mathf.Sqrt(Mathf.Sin(radAngle))));
+		Debug.Log (secondPart.ToString ());
+		Debug.Log (Mathf.Sin (2 * radAngle));
+		float distance = (vel0sq / (2 * Physics.gravity.magnitude)) * Mathf.Sin (2 * radAngle) * (secondPart);
+		Debug.Log (distance);
+		Debug.Log (distance + initialZ);
+		Debug.Log (transform.position.z + transform.position.x);
+
 	}
 
 	void Update() {
