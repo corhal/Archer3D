223637042b8ccb328c540bca91718697a0d15682
Andrey Panfilov <a.panfilov@nexters.com>
2016-08-10 11:46:36 +0300

    Unsolved mistakes

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -7,12 +7,14 @@ public class Arrow : MonoBehaviour {
 
 	private Rigidbody myRigidbody;
 	private bool isFlying;
+	Vector3 initialPosition;
 
 	void Awake() {
 		myRigidbody = GetComponent<Rigidbody> ();
 	}
 
 	void Start() {
+		initialPosition = new Vector3 (transform.position.x, 0.0f, transform.position.z);
 		isFlying = true;
 		float initialZ = transform.position.z;
 		Vector3 force = new Vector3(transform.forward.x * Force, transform.forward.y * Force, transform.forward.z * Force);
@@ -21,15 +23,15 @@ public class Arrow : MonoBehaviour {
 		float vel0sq = Mathf.Pow (force.magnitude, 2.0f);
 		float radAngle = Mathf.Deg2Rad * angle;
 		float h = transform.position.y;
-		Debug.Log (h);
+		//Debug.Log (h);
 			
 		float secondPart = 1 + Mathf.Sqrt (1 + (2 * Physics.gravity.magnitude * h) / (vel0sq * Mathf.Sqrt(Mathf.Sin(radAngle))));
-		Debug.Log (secondPart.ToString ());
-		Debug.Log (Mathf.Sin (2 * radAngle));
+		//Debug.Log (secondPart.ToString ());
+		//Debug.Log (Mathf.Sin (2 * radAngle));
 		float distance = (vel0sq / (2 * Physics.gravity.magnitude)) * Mathf.Sin (2 * radAngle) * (secondPart);
-		Debug.Log (distance);
-		Debug.Log (distance + initialZ);
-		Debug.Log (transform.position.z + transform.position.x);
+		//Debug.Log (distance);
+		//Debug.Log (distance + initialZ);
+		//Debug.Log (transform.position.z + transform.position.x);
 
 	}
 
@@ -47,5 +49,7 @@ public class Arrow : MonoBehaviour {
 			Destroy (myRigidbody);
 			//myRigidbody.useGravity = false;
 		//}
+		Vector3 myPosition = new Vector3 (transform.position.x, 0.0f, transform.position.z);
+		Debug.Log(Vector3.Distance(initialPosition, myPosition));
 	}
 }
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -7,48 +7,67 @@ public class Shooter : MonoBehaviour {
 	public GameObject ArrowPrefab;
 	public GameObject Target;
 	float g = Physics.gravity.magnitude;
-	float v0sq = 10.0f;
+	float v0sq = 100.0f;
 	float l;
 	float h;
 
 	// Use this for initialization
 	void Start () {
-		transform.rotation = Quaternion.LookRotation (new Vector3(Target.transform.position.x, 0.0f, Target.transform.position.z));
+		
+	}
+
+	void Shoot() {
+		// transform.rotation = Quaternion.LookRotation (new Vector3(Target.transform.position.x, 0.0f, Target.transform.position.z));
+
 		float radius = Target.transform.localScale.x / 2;
 		float t = 2 * Mathf.PI * Random.Range (0, radius);
 		float u = Random.Range (0, radius) + Random.Range (0, radius);
 		float r;
+
 		if (u > radius) {
 			r = 2 - u;
 		} else {
 			r = u;
 		}
+
+		Debug.Log (t);
+
+		// t = t * Mathf.Rad2Deg;
+
 		float x = r * Mathf.Cos (t);
 		float z = r * Mathf.Sin (t);
 		x = Target.transform.position.x + x;
 		z = Target.transform.position.z + z;
-		Debug.Log (x + ":" + z);
-		Vector3 distance = new Vector3 (x, transform.position.y, z);
-		l = Vector3.Distance (transform.position, distance);
+		//Debug.Log (x + ":" + z);
+
+		transform.rotation = Quaternion.LookRotation (new Vector3(x, 0.0f, z));
+
+		Vector3 distance = new Vector3 (x, ShootPoint.transform.position.y, z);
+		l = Vector3.Distance (ShootPoint.transform.position, distance);
 		h = ShootPoint.transform.position.y;
 		Debug.Log ("l: " + l + ", h: " + h);
-		FindAngle ();
+		float alpha = FindAngle ();
+		ShootPoint.transform.eulerAngles = new Vector3 (-alpha, transform.eulerAngles.y, transform.eulerAngles.z);
+		//Debug.Log (ShootPoint.transform.eulerAngles);
+		Instantiate (ArrowPrefab, ShootPoint.transform.position, ShootPoint.transform.rotation);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space)) {
-			Instantiate (ArrowPrefab, ShootPoint.transform.position, ShootPoint.transform.rotation);
+			Shoot ();
 		}	
 	}
 
-	void FindAngle() {
+	float FindAngle() {
 		float a = (g * Mathf.Pow (l, 2)) / (2 * v0sq);
 		float b = -l;
 		float c = b - h;
+		Debug.Log ("a, b, c: " + a + ":" + b + ":" + c);
 		float alpha = SquareSolve (a, b, c);
 
 		Debug.Log ("alpha: " + alpha.ToString ());
+		return alpha;
 	}
 
 	float SquareSolve(float a, float b, float c) {
@@ -59,16 +78,19 @@ public class Shooter : MonoBehaviour {
 			x1 = (-b + Mathf.Sqrt (Mathf.Pow (b, 2) - 4 * a * c)) / (2 * a);
 			x2 = (-b - Mathf.Sqrt (Mathf.Pow (b, 2) - 4 * a * c)) / (2 * a);
 		}
+		Debug.Log ("x: " + x1 + ":" + x2);
 		float alpha1 = Mathf.Atan (x1);
 		float alpha2 = Mathf.Atan (x2);
+		Debug.Log ("alphas: " + alpha1 + ":" + alpha2);
 		alpha1 = alpha1 * Mathf.Rad2Deg;
 		alpha2 = alpha2 * Mathf.Rad2Deg;
-		Debug.Log ("alpha1: " + alpha1 + ", alpha2: " + alpha2);
-		if (Mathf.Min(alpha1, alpha2) > 0) {
+		// Debug.Log ("alpha1: " + alpha1 + ", alpha2: " + alpha2);
+		return -alpha2;
+		/*if (Mathf.Min(alpha1, alpha2) > 0) {
 			return Mathf.Min (alpha1, alpha2);
 		} else {
 			return Mathf.Max (alpha1, alpha2);
-		}
+		}*/
 
 	}
 }
